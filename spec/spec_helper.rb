b5ad6a543934d78df71dd08929c2084ed6f1d782
Gregor Schmidt <schmidt@nach-vorne.eu>
2017-07-04 14:18:43 +0200

    Remove rspec-rails test dependendency
    It works just fine without out.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,9 +10,4 @@ require "rails/test_unit/railtie"
 
 require "cloudfront/rails"
 
-require "rspec/rails"
 require "webmock/rspec"
-
-RSpec.configure do |config|
-  config.infer_base_class_for_anonymous_controllers = false
-end
